078a217b4b7aff6a52f534e9029890b60f50f432
dokiskod <80366758+dokiskod@users.noreply.github.com>
2026-04-01 18:39:03 -0300

    init

---
+++ kvcutshortener/src/main/java/com/kb/kvcutshortener/KvcutshortenerApplication.java
@@ -0,0 +1,13 @@
+package com.kb.kvcutshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvcutshortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvcutshortenerApplication.class, args);
+	}
+
+}
---
+++ kvcutshortener/src/test/java/com/kb/kvcutshortener/KvcutshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.kvcutshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvcutshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
